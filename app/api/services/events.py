82c565087bd7a4f7a7c04c5415941262acbbc942
Shubhashish Verma <svashish305@gmail.com>
2023-05-04 12:28:17 +0530

    updated get_events

--- app/api/services/events.py
+++ app/api/services/events.py
@@ -11,15 +11,16 @@ async def get_events(db: Session):
     events = []
     try:
         all_events = db.query(models.Event).order_by(models.Event.id.asc()).all() or []
-        events = [
-            schemas.EventOut(
-                id=event.id, 
-                name=event.name, 
-                description=event.description, 
-                date_time=event.date_time, 
-                num_available=event.num_available
-            ) for event in all_events
-        ]
+        if len(all_events) > 0:
+            events = [
+                schemas.EventOut(
+                    id=event.id, 
+                    name=event.name, 
+                    description=event.description, 
+                    date_time=event.date_time, 
+                    num_available=event.num_available
+                ) for event in all_events
+            ]
     except Exception as e:
         print("Unable to get events due to: ", e)
     finally:

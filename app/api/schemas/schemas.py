55ea81f9ba71d6bd695e04ce5758bb8358b21edf
Shubhashish Verma <svashish305@gmail.com>
2023-05-04 11:54:52 +0530

    Refactor data models (#3)
    * simplified data modelling and updated docker containers
    
    * minor fixes
    
    * updated readme

--- app/api/schemas/schemas.py
+++ app/api/schemas/schemas.py
@@ -1,19 +1,30 @@
-from pydantic import BaseModel
 from datetime import datetime
+from enum import Enum
+from typing import Optional
+from pydantic import BaseModel
 
 class EventOut(BaseModel):
-    name: str
-    description: str
-    date_time: datetime
-    num_available_tickets: int
+    id: int = None
+    name: str = None
+    description: str = None
+    date_time: datetime = None
+    num_available: int = None
 
 class ReservationIn(BaseModel):
-    ticket_id: int
+    event_id: int
     num_tickets: int
+
+class ReservationOperation(str, Enum):
+    INCREMENT = 'increase number of tickets'
+    DECREMENT = 'decrease number of tickets'
+
+class ReservationUpdate(BaseModel):
+    num_tickets: int
+    operation: Optional[ReservationOperation] = None
 
 class ReservationOut(BaseModel):
     id: int = None
-    ticket_id: int = None
+    event_id: int = None
     num_reserved: int = None
     message: str = None
     code: int = None
--- app/api/services/events.py
+++ app/api/services/events.py
@@ -4,23 +4,23 @@ from sqlalchemy.orm import Session
 from app.api.models import models
 from app.api.schemas import schemas
 
-def is_future_event(event_date_time):
-    return event_date_time > datetime.now()
+def is_past_event(event_date_time):
+    return event_date_time < datetime.now()
 
 async def get_events(db: Session):
-    events_out = []
+    events = []
     try:
-        events = db.query(models.Event).all()
-        for event in events:
-            tickets = db.query(models.Ticket).filter(models.Ticket.event_id == event.id).all()
-            num_available_tickets = sum([ticket.num_available for ticket in tickets])
-            events_out.append(schemas.EventOut(
-                name=event.name,
-                description=event.description,
-                date_time=event.date_time,
-                num_available_tickets=num_available_tickets
-            ))
+        all_events = db.query(models.Event).order_by(models.Event.id.asc()).all() or []
+        events = [
+            schemas.EventOut(
+                id=event.id, 
+                name=event.name, 
+                description=event.description, 
+                date_time=event.date_time, 
+                num_available=event.num_available
+            ) for event in all_events
+        ]
     except Exception as e:
         print("Unable to get events due to: ", e)
     finally:
-        return events_out
+        return events
